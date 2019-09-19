da32349e96e77673f1ca480bb7423e636215920b
gnelak <gnelak@Lers-MBP-2.lan>
2019-09-20 02:09:52 +0800

    template and first edits

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://gnelak.github.io/",
+    "name": "Ler Ka Leng",
+    "role": "Storyteller, Digital Strategist, Marketer, Analyst, Social Media Specialist",
+    "linkedinId":"https://linkedin.com/in/ler-ka-leng-24304054/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Based in Singapore. 7 years of diverse experience and skills in leading projects, transforming social media experience & data analysis.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/ler-ka-leng-24304054/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/gnelak",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://skype.com",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"Social marketer, data analyst and strategist based in Singapore. 7 years of diverse experience and skills in leading projects, transforming social media experience & data analysis. Currently leading social & content marketing for Manulife Singapore. Had the privilege of starting projects from scratch at every new job, from client acquisition to leading a 2.5-year national programme handling high budget & managing 70 ongoing projects. I thrive in an open & fast paced environment by finding comfort in discomfort. I learn fast on the job with my obsessive curiosity. Independence is in my streak, but I am always a team player.",
+    "address":"Singappore",
+    "website":"https://gnelak.github.io",
+    "education":[
+      {
+        "UniversityName":"National University of Singapore",
+        "specialization":"Communications & New Media",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Hello world"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Bye world"
+      }
+    ]
+  }
+  
+  export default resumeData
