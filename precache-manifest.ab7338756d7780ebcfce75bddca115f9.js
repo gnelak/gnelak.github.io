280ef07bfe8baaa7ab24d17e3579044a2dcf4057
gnelak <gnelak@Lers-MacBook-Pro-2.local>
2019-09-21 14:23:51 +0800

    [Deploy] Update build files

---
+++ precache-manifest.ab7338756d7780ebcfce75bddca115f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4e86fd6500376fe286a792c6348974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19064230fa0d431db594",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "049671c61096ba49d771",
+    "url": "/static/js/2.f93066a5.chunk.js"
+  },
+  {
+    "revision": "19064230fa0d431db594",
+    "url": "/static/js/main.f699156a.chunk.js"
+  },
+  {
+    "revision": "159cd226c0697e17f3c7",
+    "url": "/static/js/runtime~main.d1a98e32.js"
+  }
+]);
